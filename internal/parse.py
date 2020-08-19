567ff0be598a99d03d69fed6caf70623b7285bbc
HDSQ <31365175+MiguelGuthridge@users.noreply.github.com>
2020-08-19 14:12:13 +1000

    Add event option for event detector

--- internal/parse.py
+++ internal/parse.py
@@ -13,25 +13,47 @@ class EventDetector:
     basic_controls = dict()
     
     def recognise(self, status, note):
-        id = (status, note)
-        if id in self.transport_controls:
-            return "Transport", self.transport_controls[id]
+        id_val = (status, note)
+        if id_val in self.transport_controls:
+            return "Transport", self.transport_controls[id_val]
         
-        elif id in self.fader_controls:
-            return "Fader", self.fader_controls[id]
+        elif id_val in self.fader_controls:
+            return "Fader", self.fader_controls[id_val]
 
-        elif id in self.fader_button_controls:
-            return "Fader Button", self.fader_button_controls[id]
+        elif id_val in self.fader_button_controls:
+            return "Fader Button", self.fader_button_controls[id_val]
         
-        elif id in self.knob_controls:
-            return "Knob", self.knob_controls[id]
+        elif id_val in self.knob_controls:
+            return "Knob", self.knob_controls[id_val]
         
-        # Turn this one into a for loop since it's 2D
-        elif id in self.coord_controls:
-            return "Coord", self.fader_controls[id]
+        elif id_val in self.coord_controls:
+            return "Coord", self.fader_controls[id_val]
         
-        elif id in self.basic_controls:
-            return "Basic", self.basic_controls[id]
+        elif id_val in self.basic_controls:
+            return "Basic", self.basic_controls[id_val]
+
+        else:
+            return "Unrecognised", "Null"
+    
+    def addEvent(self, status, note, event_type, control):
+        id_val = (status, note)
+        if event_type == "Transport":
+            self.transport_controls[id_val] = control
+        
+        elif event_type == "Fader":
+            self.fader_controls[id_val] = control
+        
+        elif event_type == "Fader Button":
+            self.fader_button_controls[id_val] = control
+        
+        elif event_type == "Knob":
+            self.knob_controls[id_val] = control
+        
+        elif event_type == "Coord":
+            self.coord_controls[id_val] = control
+        
+        elif event_type == "Basic":
+            self.basic_controls[id_val] = control
 
 
 detector = EventDetector()
@@ -43,7 +65,7 @@ class ParsedEvent:
         self.note = event.data1
         self.value = event.data2
         
-        
+        self.type, self.control = detector.recognise(self.status, self.note)
     
     
 
