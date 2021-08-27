8ddbd60b53a091be00c91e8503a5bc9c46c0a3be
Miguel Guthridge <hdsq@outlook.com.au>
2021-08-28 01:55:27 +1000

    ✏CHANGE: Remove old deviceconfig code

--- deviceconfig/launchkeymk2/config.py
+++
@@ -1,9 +0,0 @@
-""" deviceconfig > _template > config.py
-
-This file contains configuration options for your controller's interface modules
-
-Edit it to ensure your device works properly
-"""
-
-# Add device IDs of supported devices
-SUPPORTED_DEVICE_IDS = ["0020297c000000"]
