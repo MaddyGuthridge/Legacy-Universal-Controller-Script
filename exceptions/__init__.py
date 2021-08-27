db9bc86631da8b001fb46fba66f6a06db7fcaaea
Miguel Guthridge <hdsq@outlook.com.au>
2021-08-28 01:04:50 +1000

    ✨NEW: Create DeviceSetupException

--- exceptions/__init__.py
+++ exceptions/__init__.py
@@ -5,5 +5,6 @@ Contains definitions for exceptions used within the script
 Author: Miguel Guthridge
 """
 
+from .setup_exception import DeviceSetupException
 from .midirecognise import MidiRecogniseException
 from .midiout import MidiOutException
---
+++ exceptions/setup_exception.py
@@ -0,0 +1,10 @@
+"""setup_exception.py
+
+Contains definition for DeviceSetupException
+
+Author: Miguel Guthridge
+"""
+
+class DeviceSetupException(Exception):
+    """Raised when an error occurred during device setup.
+    """
